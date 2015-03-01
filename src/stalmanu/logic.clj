4a348f5617f9cdfab6d28b8da102e8cf25b4050c
emiln <emil@nauerby.com>
2015-03-01 22:23:37 +0100

    Add interjection logic
    Stalmanu will now interject when he notices obviously incorrect references
    to "linux" without a preceding "GNU". He's not very clever yet, though.

---
+++ src/stalmanu/logic.clj
@@ -0,0 +1,11 @@
+(ns stalmanu.logic
+	(:require [clojure.string :as s]))
+
+(defn interject?
+	"Returns true if the string definitely warrants an interjection."
+	[string]
+	(let [lower  (s/lower-case (or string ""))
+				linux  (.indexOf lower "linux")
+				gahnoo (.indexOf lower "gnu")]
+		(and (> linux -1)
+				 (= gahnoo -1))))
--- src/stalmanu/run.clj
+++ src/stalmanu/run.clj
@@ -2,12 +2,15 @@
   (:require
     [clojure.core.async :as async]
     [clojure.data.json :as json]
-    [stalmanu.client :as client]))
+    [stalmanu.client :as client]
+    [stalmanu.logic :refer [interject?]]))
 
 (defn start!
   [token]
   (let [{:keys [receive send]} (client/login! token)]
     (async/go-loop []
       (when-let [msg (async/<! receive)]
-        (println (json/read-str msg :key-fn keyword))
-        (recur)))))
+        (let [text (:text (json/read-str msg :key-fn keyword))]
+          (when (interject? text)
+            (async/>! send "It's GNU+Linux, faggot."))
+          (recur))))))

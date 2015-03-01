c6698aee17619b2057fe6e19863dd24f90abb6ed
emiln <emil@nauerby.com>
2015-03-01 22:49:34 +0100

    Update the interjection
    The new one is less profane and more verbose as seems fitting.

--- src/stalmanu/run.clj
+++ src/stalmanu/run.clj
@@ -5,6 +5,11 @@
     [stalmanu.client :as client]
     [stalmanu.logic :refer [interject?]]))
 
+(def interjection
+  (str "I'd just like to interject for a moment. What you're referring"
+    " to as linux is in fact GNU/Linux, or as I've recently taken to"
+    " calling it, GNU plus Linux."))
+
 (defn start!
   [token]
   (let [{:keys [receive send]} (client/login! token)]
@@ -12,5 +17,5 @@
       (when-let [msg (async/<! receive)]
         (let [text (:text (json/read-str msg :key-fn keyword))]
           (when (interject? text)
-            (async/>! send "It's GNU+Linux, faggot."))
+            (async/>! send interjection))
           (recur))))))

669eb43ebc2719bfd83475e468f4763f073e8e28
Emil Holm Nauerby <emil@nauerby.com>
2015-03-03 08:28:01 +0100

    Th-thanks, Atom!

--- test/logic_test.clj
+++ test/logic_test.clj
@@ -1,19 +1,19 @@
 (ns logic-test
-	(:require [expectations :refer [expect]]
-						[stalmanu.logic :refer [interject?]]))
+  (:require [expectations   :refer [expect]]
+            [stalmanu.logic :refer [interject?]]))
 
 ;; We don't want Stalmanu spamming the channel, so we require every one of the
 ;; following to fail.
 
 (expect false
-	(interject? "You should be allowed to say GNU/Linux"))
+  (interject? "You should be allowed to say GNU/Linux"))
 (expect false
-	(interject? "And definitely also GNU+Linux"))
+  (interject? "And definitely also GNU+Linux"))
 (expect false
-	(interject? "Hell, even GNU and Linux should be fine."))
+  (interject? "Hell, even GNU and Linux should be fine."))
 (expect false
-	(interject? "More creative phrases like 'gnu-infested linux' should work."))
+  (interject? "More creative phrases like 'gnu-infested linux' should work."))
 (expect false
-	(interject? "Mentioning the Linux kernel should also be dandy."))
+  (interject? "Mentioning the Linux kernel should also be dandy."))
 (expect false
-	(interject? "As far as kernels go, Linux is pretty good."))
+  (interject? "As far as kernels go, Linux is pretty good."))
